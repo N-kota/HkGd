35e88835c26ee0dd2b3c8d20d3214ba67941069b
N-kota <taukoman1025@gmail.com>
2021-09-03 22:49:18 +0900

    予め用意したslackのチャンネルにテキストを送信することは出来るようになりました。入力内容から動的にURLを生成する方法がいまいち探せんかった()

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hackathonguild.apps.HackathonguildConfig',
+    'django_slack',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,7 @@ EMAIL_HOST_USER = 'ibguild2021'
 EMAIL_HOST_PASSWORD = 'Hackathon2109'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# デバッグ用：以下の設定だとコンソールに出力される
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SLACK_WEBHOOK_ENDPOINT = 'https://hooks.slack.com/services/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'
