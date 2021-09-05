0f15c376559b850e3f36f67a7000d88ea72a71da
Ryo <hana05ry@icloud.com>
2021-09-05 10:20:22 +0900

    フォームに入力チェック（バリデーション）を追加

--- hackathonguild/models.py
+++ hackathonguild/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     hackathon_date = models.DateField('ハッカソン開催日')
     recluting_headcount = models.IntegerField('募集人数')
     delete_key = models.CharField('削除キー', max_length=8)
-    product_brief = models.TextField('プロダクト概要', max_length=500)
+    product_brief = models.TextField('プロダクト概要', max_length=500, blank=True)
     file = models.FileField('説明資料', blank=True)
     posted_date = models.DateTimeField('投稿日', default=timezone.now)
     post_delete_date = models.DateTimeField('投稿削除日', blank=True)
@@ -26,7 +26,7 @@ class Post(models.Model):
 class Participant(models.Model):
     participant_name = models.CharField('参加者名', max_length=50)
     participant_mail = models.EmailField('参加者メールアドレス', max_length=50)
-    enthusiasm = models.TextField('意気込み', max_length=200)
+    enthusiasm = models.TextField('意気込み', max_length=200, blank=True)
     participate_product_id = models.IntegerField('参加プロダクトid')
     participate_date = models.DateField('参加日', default=timezone.now)
     token = models.CharField('トークン', default='', max_length=200)
